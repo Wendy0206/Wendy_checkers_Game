ad96bad81ac405ad63f45df7a3afde2453c3c65a
Wendy0206 <155206385+Wendy0206@users.noreply.github.com>
2024-02-28 17:20:56 +0000

    50% done

--- src/js/component/tictactoe.jsx
+++ src/js/component/tictactoe.jsx
@@ -1,48 +1,71 @@
+import { element } from "prop-types";
 import React from "react";
 import { useState, useEffect } from "react";
 
 
 //create your first component
 export const TicTacToe= ()=> {
-    const [boardPosition, setBoardPosition] = useState(['','','','','','','','','']);
-    const [boardValue, setBoard] = useState([0,0,0,0,0,0,0,0,0]);
+    const [boardValue, setBoardValue] = useState(['','','','','','','','','']);
     const [countMove, setCountMove] = useState(0);
     const [MoveSymbol, setMoveSymbol] = useState(0);
-    const [cellStattus, setCellStatus] = useState(0);
-let arrayPo=[];
+    const [cellStatus, setCellStatus] = useState([0,0,0,0,0,0,0,0,0]);
+let arrayStatus=[0,0,0,0,0,0,0,0,0];
+ // let arrayValue=['','','','','','','','',''];
     useEffect(()=>{
-setBoardPosition(['','','','','','','','','']);
+
 
 },[]);
 
 function Move_c11 (index){
-setCountMove(countMove+1);
-if(countMove%2!=0){
-setMoveSymbol('o');
-}
-if(countMove%2==0){
-    setMoveSymbol('x');
-}
-arrayPo[index]=
-setBoardPosition([index]);
+
+// verify the statusof the cell
+    if(cellStatus[index]==1)
+    {
+      alert('You cant play again here');
+        
+    }
+    else{
+        arrayStatus[index]=1;
+        setCellStatus(arrayStatus);
+        console.log(cellStatus);
+       
+        if(countMove%2!=0){
+        setMoveSymbol('O');
+      //   arrayValue[index]='o';
+     //  setBoardValue(boardValue.map((ind,element)=>ind==index? 'O':element));
+          //  setBoardValue([]);
+            console.log(index);
+            //console.log(boardValue);
 
 
-console.log('This is the  cell '+index+' that just moved');
+        }
+        if(countMove%2==0){
+            setMoveSymbol('X');
+           //  setBoardValue(boardValue.map((ind,element)=>ind==2? 'X': element));
+           // console.log(boardValue);
+            console.log(index);
+           
+        }
+        setCountMove(countMove+1);
+       
+       
+    }
+
 
 } 
 
 	return	(
         <div class="container">
        <div className="theBoard">
-        <div className="c1 white"><span onClick={()=>Move_c11(1)} ></span></div>
-        <div className="c12 black"><span onClick={()=>Move_c11(2)} >naknk</span></div>
-        <div className="c13 white"><span onClick={()=>Move_c11(3)}>efnwenf</span></div>
-        <div className="c21 black"><span onClick={()=>Move_c11(4)}>wkefwek</span></div>
-        <div className="c22 white"><span onClick={()=>Move_c11(5)}>qekfek</span></div>
-        <div className="c23 black"><span onClick={()=>Move_c11(6)}>efnkfn</span></div>
-        <div className="c31 white"><span onClick={()=>Move_c11(7)}>kdfnwek</span></div>
-        <div className="c32 black"><span onClick={()=>Move_c11(8)}>qekfnk</span></div>
-        <div className="c33 white"><span onClick={()=>Move_c11(9)}>kfneef</span></div>
+        <div className="c1 white" ><span onClick={()=>Move_c11(0)}>{boardValue[0]}</span></div>
+        <div className="c12 black" ><span onClick={()=>Move_c11(1)}>{boardValue[1]}</span></div>
+        <div className="c13 white"><span onClick={()=>Move_c11(2)}>{boardValue[2]}</span></div>
+        <div className="c21 black"><span onClick={()=>Move_c11(3)}>{boardValue[3]}</span></div>
+        <div className="c22 white"><span onClick={()=>Move_c11(4)}>{boardValue[4]}</span></div>
+        <div className="c23 black"><span onClick={()=>Move_c11(5)}>{boardValue[5]}</span></div>
+        <div className="c31 white"><span onClick={()=>Move_c11(6)}>{boardValue[6]}</span></div>
+        <div className="c32 black"><span onClick={()=>Move_c11(7)}>{boardValue[7]}</span></div>
+        <div className="c33 white"><span onClick={()=>Move_c11(8)}>{boardValue[8]}</span></div>
 
        </div>
   
