5eebef4dd900f4183989fc529dff193daaa51563
Wendy0206 <155206385+Wendy0206@users.noreply.github.com>
2024-02-29 15:13:10 +0000

    Player's name added

--- src/js/component/tictactoe.jsx
+++ src/js/component/tictactoe.jsx
@@ -11,7 +11,8 @@ export const TicTacToe= ()=> {
     const [cellStatus, setCellStatus] = useState([2,2,2,2,2,2,2,2,2]);
     const [winnerE, setWinnerE] = useState('visible');
     const [winnerF, setWinnerF] = useState('hidden');
-   
+    const [player, setPlayer] = useState(['Player 1'],['Player 2']);
+    const [finalplayer, setFinalPlayer] = useState(['']);
 
     useEffect(()=>{
       
@@ -24,11 +25,21 @@ export const TicTacToe= ()=> {
       let diagonal=  boardValue[0]+boardValue[4]+boardValue[8];
       let diagonal2=  boardValue[2]+boardValue[4]+boardValue[6];
 
- 
+      if(countMove==1){
+
+        let new1=prompt("Please tell us who is playing right now");
+          setPlayer(player.map((element,ind)=>ind==0? new1: element));
+           }
+           if(countMove==2){
+             let new2=prompt("Please tell us who is playing right now");
+               setPlayer(player.map((element,ind)=>ind==1? new2: element));
+                }
+
     if(countMove>4 && (verticalW=='XXX' || verticalW2=='XXX' || verticalW3=='XXX' || horizontalW=='XXX' || horizontalW2=='XXX' || horizontalW3=='XXX' || diagonal=='XXX'|| diagonal2=='XXX')){
     
        // alert('You win');
         setCountMove(0);
+        setFinalPlayer(player[0]);
         setCellStatus(cellStatus.map((element,ind)=>(ind==ind)? 2 : element));
         setBoardValue(boardValue.map((element,ind)=> (ind==ind)? '': element));  
         setWinnerE('hidden');
@@ -43,6 +54,7 @@ else if(countMove>4 && (verticalW=='OOO' || verticalW2=='OOO' || verticalW3=='OO
     setBoardValue(boardValue.map((element,ind)=> (ind==ind)? '': element));  
     setWinnerE('hidden');
    setWinnerF('visible');
+   setFinalPlayer(player[1]);
 }
 else if(countMove==9){
   
@@ -122,9 +134,12 @@ function Move_cell (index){
         <div className="c33 white"><span onClick={()=>Move_cell(8)}>{boardValue[8]}</span></div>
 
        </div>
+
        <div className="img_div" style={{visibility: winnerF}} >
+        <h1>{finalplayer}  has won</h1>
 		<img src={Win} alt="test image"/>
 		</div>
+
         <div className="button_div" >
 		<button type="button" class="btn btn-info" onClick={()=>Reset_board()}>Start Over</button>
 		</div>
