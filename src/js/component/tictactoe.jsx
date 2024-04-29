f3db9a15eaeedab91c3ce6d0fd2dffee9857bed8
Wendy0206 <155206385+Wendy0206@users.noreply.github.com>
2024-04-29 16:58:44 +0000

    board done

--- src/js/component/tictactoe.jsx
+++ src/js/component/tictactoe.jsx
@@ -18,65 +18,7 @@ export const TicTacToe= ()=> {
     
 
     useEffect(()=>{
-      
-      let verticalW= boardValue[0]+boardValue[1]+boardValue[2];
-      let verticalW2=  boardValue[3]+boardValue[4]+boardValue[5];
-      let verticalW3=  boardValue[6]+boardValue[7]+boardValue[8];
-      let horizontalW= boardValue[0]+boardValue[3]+boardValue[6];
-      let horizontalW2=  boardValue[1]+boardValue[4]+boardValue[7];
-      let horizontalW3=  boardValue[2]+boardValue[5]+boardValue[8];
-      let diagonal=  boardValue[0]+boardValue[4]+boardValue[8];
-      let diagonal2=  boardValue[2]+boardValue[4]+boardValue[6];
-
-      if(countMove==1 && player[0]==''){
-        
-          let new1=prompt("Please tell us who is playing right now");
-          setPlayer(player.map((element,ind)=>ind==0? new1: element));
-        }
-      
-
-           if(countMove==2 && player[1]==''){
-              let new2=prompt("Please tell us who is playing right now");
-              setPlayer(player.map((element,ind)=>ind==1? new2: element));
-              setPlayerName('visible');
-              
-               }
-
-       
-
-
-
-    if(verticalW=='XXX' || verticalW2=='XXX' || verticalW3=='XXX' || horizontalW=='XXX' || horizontalW2=='XXX' || horizontalW3=='XXX' || diagonal=='XXX'|| diagonal2=='XXX'){
     
-       // alert('You win');
-        setCountMove(0);
-        setFinalPlayer(player[0]);
-        setCellStatus(cellStatus.map((element,ind)=>(ind==ind)? 2 : element));
-        setBoardValue(boardValue.map((element,ind)=> (ind==ind)? '': element));  
-        setWinnerE('hidden');
-        setWinnerF('visible');
-        setPlayerName('hidden');
-      
-}
-else if(verticalW=='OOO' || verticalW2=='OOO' || verticalW3=='OOO' || horizontalW=='OOO' || horizontalW2=='OOO' || horizontalW3=='OOO' || diagonal=='OOO'|| diagonal2=='OOO'){
-    
-  //  alert('Someone wins');
-    setCountMove(0);
-    setCellStatus(cellStatus.map((element,ind)=>(ind==ind)? 2 : element));
-    setBoardValue(boardValue.map((element,ind)=> (ind==ind)? '': element));  
-    setWinnerE('hidden');
-   setWinnerF('visible');
-   setPlayerName('hidden');
-   setFinalPlayer(player[1]);
-}
-else if(countMove==9){
-  
-    setCellStatus(cellStatus.map((element,ind)=>(ind==ind)? 2 : element));
-    setBoardValue(boardValue.map((element,ind)=> (ind==ind)? '': element)); 
-    setWinnerE('visible');
-    setWinnerF('hidden');
-}
-
 
 
 
@@ -116,51 +58,99 @@ function Move_cell (index){
 function reset_board (){
   setWinnerE('visible');
   setWinnerF('hidden');
-  setCellStatus(cellStatus.map((element,ind)=>(ind==ind)? 2 : element));
-  setBoardValue(boardValue.map((element,ind)=> (ind==ind)? '': element)); 
-  setPlayerName('visible');
-  setRoundCount("again ");
+  
 
 }
 
 function reset_player (){
-setWinnerE('visible');
-setWinnerF('hidden');
-setPlayerName('hidden');
-setCellStatus(cellStatus.map((element,ind)=>(ind==ind)? 2 : element));
-setBoardValue(boardValue.map((element,ind)=> (ind==ind)? '': element)); 
-setPlayer(player.map((element,ind)=> (ind==ind)? '': element));  
-setRoundCount("");
+
 }
   
    
 
 	return	(
         <div class="container">
-       <h3 style={{visibility: playerName}}>{player[0]} and {player[1]} are playing {roundCount}.</h3>
-       <div className="theBoard" style={{visibility: winnerE}} >
+         <div class="theBoard" style={{visibility: winnerE}} >
      
-        <div className="c1 white" ><span onClick={()=>Move_cell(0)}>{boardValue[0]}</span></div>
-        <div className="c12 black" ><span onClick={()=>Move_cell(1)}>{boardValue[1]}</span></div>
-        <div className="c13 white"><span onClick={()=>Move_cell(2)}>{boardValue[2]}</span></div>
-        <div className="c21 black"><span onClick={()=>Move_cell(3)}>{boardValue[3]}</span></div>
-        <div className="c22 white"><span onClick={()=>Move_cell(4)}>{boardValue[4]}</span></div>
-        <div className="c23 black"><span onClick={()=>Move_cell(5)}>{boardValue[5]}</span></div>
-        <div className="c31 white"><span onClick={()=>Move_cell(6)}>{boardValue[6]}</span></div>
-        <div className="c32 black"><span onClick={()=>Move_cell(7)}>{boardValue[7]}</span></div>
-        <div className="c33 white"><span onClick={()=>Move_cell(8)}>{boardValue[8]}</span></div>
-
+         <div class=" black" ><span onClick={()=>Move_cell(1)}></span></div>
+        <div class=" white"><span onClick={()=>Move_cell(2)}></span></div>
+        <div class=" black"><span onClick={()=>Move_cell(3)}></span></div>
+        <div class=" white"><span onClick={()=>Move_cell(4)}></span></div>
+    
+        <div class=" black"><span onClick={()=>Move_cell(5)}></span></div>
+        <div class=" white"><span onClick={()=>Move_cell(6)}></span></div>
+        <div class=" black"><span onClick={()=>Move_cell(7)}></span></div>
+        <div class=" white"><span onClick={()=>Move_cell(8)}></span></div>
+        <div class=" white"><span onClick={()=>Move_cell(9)}></span></div>
+        <div class=" black"><span onClick={()=>Move_cell(10)}></span></div>
+        <div class=" white"><span onClick={()=>Move_cell(11)}></span></div>
+        <div class=" black"><span onClick={()=>Move_cell(12)}></span></div>
+        <div class=" white"><span onClick={()=>Move_cell(13)}></span></div>
+        <div class=" black"><span onClick={()=>Move_cell(14)}></span></div>
+        <div class=" white"><span onClick={()=>Move_cell(15)}></span></div>
+        <div class=" black"><span onClick={()=>Move_cell(16)}></span></div>
+        <div class=" black"><span onClick={()=>Move_cell(17)}></span></div>
+    
+        <div class="white"><span onClick={()=>Move_cell(18)}></span></div>
+        <div class=" black"><span onClick={()=>Move_cell(19)}></span></div>
+        <div class=" white"><span onClick={()=>Move_cell(20)}></span></div>
+        <div class=" black" ><span onClick={()=>Move_cell(21)}></span></div>
+        <div class=" white" ><span onClick={()=>Move_cell(22)}></span></div>
+        <div class=" black"><span onClick={()=>Move_cell(23)}></span></div>
+        <div class=" white"><span onClick={()=>Move_cell(24)}></span></div>
+        <div class=" white"><span onClick={()=>Move_cell(25)}></span></div>
+    
+        <div class=" black"><span onClick={()=>Move_cell(26)}></span></div>
+        <div class=" white"><span onClick={()=>Move_cell(27)}></span></div>
+        <div class=" black"><span onClick={()=>Move_cell(28)}></span></div>
+        <div class=" white"><span onClick={()=>Move_cell(29)}></span></div>
+        <div class=" black"><span onClick={()=>Move_cell(30)}></span></div>
+        <div class=" white"><span onClick={()=>Move_cell(31)}></span></div>
+        <div class=" black"><span onClick={()=>Move_cell(32)}></span></div>
+        <div class=" black"><span onClick={()=>Move_cell(33)}></span></div>
+        <div class=" white"><span onClick={()=>Move_cell(34)}></span></div>
+        <div class=" black"><span onClick={()=>Move_cell(35)}></span></div>
+        <div class=" white"><span onClick={()=>Move_cell(36)}></span></div>
+        <div class=" black"><span onClick={()=>Move_cell(37)}></span></div>
+        <div class=" white"><span onClick={()=>Move_cell(38)}></span></div>
+        <div class=" black"><span onClick={()=>Move_cell(39)}></span></div>
+        <div class=" white"><span onClick={()=>Move_cell(40)}></span></div>
+        <div class=" white" ><span onClick={()=>Move_cell(41)}></span></div>
+        <div class=" black" ><span onClick={()=>Move_cell(42)}></span></div>
+        <div class=" white"><span onClick={()=>Move_cell(43)}></span></div>
+        <div class=" black"><span onClick={()=>Move_cell(44)}></span></div>
+        <div class=" white"><span onClick={()=>Move_cell(45)}></span></div>
+    
+        <div class=" black"><span onClick={()=>Move_cell(46)}></span></div>
+        <div class=" white"><span onClick={()=>Move_cell(47)}></span></div>
+        <div class=" black"><span onClick={()=>Move_cell(48)}></span></div>
+        <div class=" black"><span onClick={()=>Move_cell(49)}></span></div>
+        <div class=" white"><span onClick={()=>Move_cell(50)}></span></div>
+       
+        <div class=" black"><span onClick={()=>Move_cell(51)}></span></div>
+        <div class=" white"><span onClick={()=>Move_cell(52)}></span></div>
+        <div class=" black"><span onClick={()=>Move_cell(53)}></span></div>
+        <div class=" white"><span onClick={()=>Move_cell(54)}></span></div>
+        <div class=" black"><span onClick={()=>Move_cell(55)}></span></div>
+        <div class=" white"><span onClick={()=>Move_cell(56)}></span></div>
+        <div class=" white"><span onClick={()=>Move_cell(57)}></span></div>
+        <div class=" black"><span onClick={()=>Move_cell(58)}></span></div>
+        <div class=" white"><span onClick={()=>Move_cell(59)}></span></div>
+        <div class=" black"><span onClick={()=>Move_cell(60)}></span></div>
+        <div class=" white"><span onClick={()=>Move_cell(61)}></span></div>
+        <div class=" black"><span onClick={()=>Move_cell(62)}></span></div>
+        <div class=" white"><span onClick={()=>Move_cell(63)}></span></div>
+        <div class=" black"><span onClick={()=>Move_cell(64)}></span></div>
        </div>
 
-       <div className="img_div" style={{visibility: winnerF}} >
-       <h1>{finalplayer} has won .</h1>
-		<img src={Win} alt="test image"/>
-		</div>
+    
 
-        <div className="button_div" >
-		<button type="button " class="btn btn-info bg-success" onClick={()=>reset_board()}>Lets play again</button>
+        <div class="button_div" >
+		{/* <button type="button " class="btn btn-info bg-success" onClick={()=>reset_board()}>Lets play again</button>
     <button type="button " class="btn btn-info bg-secondary" onClick={()=>reset_player()}>Play With someone else</button>
-		</div>
+		*/}
+
+</div> 
   
 </div>
     );
