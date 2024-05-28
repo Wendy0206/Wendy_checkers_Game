adba16bd010b21c8786a5199cf693cb884ac395a
Wendy Donley Pierre <155206385+Wendy0206@users.noreply.github.com>
2024-05-28 16:47:54 +0000

    checkers board, initiliazed and reset done

--- src/js/component/checkers.jsx
+++ src/js/component/checkers.jsx
@@ -10,32 +10,67 @@ export const Checkers= ()=> {
       
 
     useEffect(()=>{
-    let reset_board=[];
-  for (let i=1; i<65;i++){
-   let newObj={};
-   if (i%2!=0){
-    newObj.classN='dark_brown';
-    if((i-1)%2!=0)
-      {
-        newObj.classN='dark_brown';
-      }
-    newObj.classN='dark_brown';
-   }
+    let reset_board=[{classN: 'dark_brown', i:0,checker:'O'}, {classN: 'dark_brown', i:1,checker:'O'}];
+   
+  for (let i=2; i<65;i++){
+    let newObj={};
 
+
+  if(reset_board[i-1].classN=='dark_brown')
+    {
+if((i-1)%8==0){
+  newObj.classN='dark_brown';
+  if(i<24){
+    newObj.checker='O';
+  }
+  if(i>40){
+    newObj.checker='X';
+  }
+  
+
+  
+}
 else{
   newObj.classN='light_brown';
 }
+    }
+
+    else{
+
+      if((i-1)%8==0){
+        newObj.classN='light_brown';
+      }
+      else{
+        newObj.classN='dark_brown';
+        if(i<24){
+          newObj.checker='O';
+        }
+        if(i>40){
+          newObj.checker='X';
+        }
+        
+      }
+      
+    }
+
+
+
 newObj.position=i; 
 reset_board.push(newObj);
 
   }  
+
+  
 setBoardValue(reset_board);
 
-
-},[countMove]);
+},[]);
       
           
-function Move_cell (index){
+function Move_cell (position){
+  if(boardValue[position].checker){
+
+
+  }
 }
  
    
@@ -43,20 +78,16 @@ function Move_cell (index){
 	return	(
 
         <div class="container">
-
          <div class="theBoard"  >
 
-         {boardValue.map((board)=>
- <div class={board.classN} ><span onClick={()=>Move_cell(board.position)}></span></div>
+         {boardValue.slice(1).map((board)=>
+ <div class={board.classN} ><span onClick={()=>Move_cell(board.position)}>{board.checker}</span></div>
 
 )}
      
        </div>
 
-  
-        <div class="button_div" >
-	
-</div> 
+
   
 </div>
 
